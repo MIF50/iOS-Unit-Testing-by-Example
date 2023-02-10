e30cb90f878847674d2674590037fe917e275cc0
mohamed ibrahim <mohamedibrahim1223@gmail.com>
2023-02-10 21:14:34 +0300

    create TestingAppDelegate for replace with AppDelegate in testing

--- AppLaunch/AppLaunchTests/TestingAppDelegate.swift
+++ AppLaunch/AppLaunchTests/TestingAppDelegate.swift
@@ -5,4 +5,13 @@
 //  Created by Mohamed Ibrahim on 10/02/2023.
 //
 
-import Foundation
+import UIKit
+
+@objc(TestingAppDelegate)
+class TestingAppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        print(">> Launching with testing app delegate")
+        return true
+    }
+}
