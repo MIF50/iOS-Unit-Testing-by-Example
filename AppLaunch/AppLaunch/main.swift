044265b587445710914650399c02f02223e2fd79
mohamed ibrahim <mohamedibrahim1223@gmail.com>
2023-02-10 21:22:13 +0300

    Removing @main form AppDelegate and replace it with main router that replace To TestingAppDelegate when run unit testing ,and real app with AppDelegate

---
+++ AppLaunch/AppLaunch/main.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  AppLaunch
+//
+//  Created by Mohamed Ibrahim on 10/02/2023.
+//
+
+import UIKit
+
+let appDelegateClass: AnyClass = NSClassFromString("TestingAppDelegate") ?? AppDelegate.self
+
+UIApplicationMain(
+    CommandLine.argc,
+    CommandLine.unsafeArgv,
+    nil,
+    NSStringFromClass(appDelegateClass)
+)
