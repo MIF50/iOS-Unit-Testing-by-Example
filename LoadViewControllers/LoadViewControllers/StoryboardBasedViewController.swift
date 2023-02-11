89b0337cd47aa6fa1ff63ac478b6024873cbcc0e
mohamed ibrahim <mohamedibrahim1223@gmail.com>
2023-02-11 10:22:05 +0300

    create storyboard view controller

---
+++ LoadViewControllers/LoadViewControllers/StoryboardBasedViewController.swift
@@ -0,0 +1,17 @@
+//
+//  StoryboardBasedViewController.swift
+//  LoadViewControllers
+//
+//  Created by Mohamed Ibrahim on 11/02/2023.
+//
+
+import UIKit
+
+class StoryboardBasedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
