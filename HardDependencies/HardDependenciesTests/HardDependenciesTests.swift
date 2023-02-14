1f02de65f42942e3e46a0dd1e0b6cc777bb35e3a
mohamed ibrahim <mohamedibrahim1223@gmail.com>
2023-02-14 21:00:11 +0300

    create empty project for hard dependencies

---
+++ HardDependencies/HardDependenciesTests/HardDependenciesTests.swift
@@ -0,0 +1,13 @@
+//
+//  HardDependenciesTests.swift
+//  HardDependenciesTests
+//
+//  Created by Mohamed Ibrahim on 14/02/2023.
+//
+
+import XCTest
+
+final class HardDependenciesTests: XCTestCase {
+
+
+}
