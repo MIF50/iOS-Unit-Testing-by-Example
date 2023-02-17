9f9007e5b52fdec6d7e00b8ea76bc0824b864027
mohamed ibrahim <mohamedibrahim1223@gmail.com>
2023-02-17 22:29:50 +0300

    create empty project for alerts

---
+++ Alert/Alert/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  Alert
+//
+//  Created by Mohamed Ibrahim on 17/02/2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
