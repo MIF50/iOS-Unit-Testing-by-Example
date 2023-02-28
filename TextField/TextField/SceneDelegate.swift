32c6426099d14458fc0c60807de1feeec99b3adc
mohamed ibrahim <mohamedibrahim1223@gmail.com>
2023-02-28 23:24:21 +0300

    create empty project for testing  test field

---
+++ TextField/TextField/SceneDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  SceneDelegate.swift
+//  TextField
+//
+//  Created by Mohamed Ibrahim on 28/02/2023.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+    
+}
+
---
+++ TextField/TextField/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  TextField
+//
+//  Created by Mohamed Ibrahim on 28/02/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
