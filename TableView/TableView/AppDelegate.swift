a66d58e911f59fe895fc470ff5786fdec8a1772a
mohamed ibrahim <mohamedibrahim1223@gmail.com>
2023-03-02 06:05:34 +0300

    create empty project for testing table view

---
+++ TableView/TableView/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  TableView
+//
+//  Created by Mohamed Ibrahim on 02/03/2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
