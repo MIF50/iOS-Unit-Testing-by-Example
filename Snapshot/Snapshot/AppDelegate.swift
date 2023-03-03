5ee276a2190a41412c84b95e9e3ead571f0c146d
mohamed ibrahim <mohamedibrahim1223@gmail.com>
2023-03-03 11:03:31 +0300

    create empty project to testing snapshot ui

---
+++ Snapshot/Snapshot/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  Snapshot
+//
+//  Created by Mohamed Ibrahim on 03/03/2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ Snapshot/Snapshot/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Snapshot
+//
+//  Created by Mohamed Ibrahim on 03/03/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
