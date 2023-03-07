7698aebf6cd2f9211021c98ac668c976af12791d
mohamed ibrahim <mohamedibrahim1223@gmail.com>
2023-03-07 22:16:24 +0300

    make demo app with unit testing

--- Refactoring/Refactoring/ViewController.swift
+++ Refactoring/Refactoring/ViewController.swift
@@ -8,10 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        super.prepare(for: segue, sender: sender)
+        
+        if segue.identifier == "changePassword" {
+            let changePasswordVC = segue.destination as? ChangePasswordViewController
+            changePasswordVC?.securityToken = "TOKEN"
+        }
     }
 }
 
